92ec9c978034000ed1596f47c09f0343beead805
Thirawoot-Put <thirawoot.put@gmail.com>
2024-12-24 22:33:29 +0700

    variable constant and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    //let x = 5;
+    //println!("value of x: {x}");
+
+    //x = 6;
+    //println!("value of x: {x}");
+    // ^^^ compile error ^^^
+
+    //let mut x = 5;
+    //println!("value of x: {x}");
+
+    //x = 6;
+    //println!("value of x: {x}");
+
+    //const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    //println!("This is constant: {THREE_HOURS_IN_SECONDS}")
+
+    // ----------------- shadowing -----------------------
+    let x = 5;
+    println!("value of x: {x}");
+
+    {
+        let x = x * 2;
+        println!("value of x: {x}");
+    }
+
+    println!("value of x: {x}");
+
+    let x = 8;
+    println!("value of x: {x}");
+
+    let space = "   ";
+    let space = space.len();
+    println!("space: {space}")
 }
