2e39b5968d18d536005c32aeb187d5298ff41581
Thirawoot.P <thirawoot.put@gmail.com>
2024-12-27 18:29:31 +0700

    scope

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
     // **** String type ****
+    //let mut s = String::from("hello");
+    //
+    //s.push_str(", world!"); // push_str() appends a literal to a String
+    //
+    //println!("{s}"); // This will print `hello, world!`
+
+    let s1 = String::from("hello");
+    let s2 = {
+        println!("{s1}");
+        s1
+    };
+
+    println!("{s2}, world!");
+    //println!("{s1}, world!"); // compile error: s1 is out of scope; Rust call dropp function;
+    //Rust invalidates s1; cannot use it any more
+
     let mut s = String::from("hello");
+    s = String::from("ahoy");
 
-    s.push_str(", world!"); // push_str() appends a literal to a String
-
-    println!("{s}"); // This will print `hello, world!`
+    println!("{s}, world!");
 }
