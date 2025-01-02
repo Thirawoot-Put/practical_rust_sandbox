6e6c242a3182cd3705cc473e39f0c96f6990aa7c
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-02 11:12:16 +0700

    ownership and function

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,69 @@
+//fn main() {
+//    // **** String type ****
+//    //let mut s = String::from("hello");
+//    //
+//    //s.push_str(", world!"); // push_str() appends a literal to a String
+//    //
+//    //println!("{s}"); // This will print `hello, world!`
+//
+//    //let s1 = String::from("hello");
+//    //let s2 = {
+//    //    println!("{s1}");
+//    //    s1
+//    //};
+//    //
+//    //println!("{s2}, world!");
+//    //println!("{s1}, world!"); // compile error: s1 is out of scope; Rust call dropp function;
+//    //Rust invalidates s1; cannot use it any more
+//
+//    //let mut s = String::from("hello");
+//    //s = String::from("ahoy");
+//    //
+//    //println!("{s}, world!");
+//
+//    //let mut s = String::from("hello");
+//    //println!("before: {s}, world!");
+//    //
+//    //s = String::from("ahoy");
+//    //
+//    //println!("after: {s}, world!");
+//
+//    //let s1 = String::from("hello");
+//    //let s2 = s1.clone();
+//    //
+//    //println!("s1 = {s1}, s2 = {s2}");
+//
+//    let x = 5;
+//    let y = x;
+//
+//    println!("x = {x}, y = {y}");
+//}
+
 fn main() {
-    // **** String type ****
-    //let mut s = String::from("hello");
-    //
-    //s.push_str(", world!"); // push_str() appends a literal to a String
-    //
-    //println!("{s}"); // This will print `hello, world!`
+    let s = String::from("hello"); // s comes into scope
+    println!("before into funciton scope: {s}");
 
-    //let s1 = String::from("hello");
-    //let s2 = {
-    //    println!("{s1}");
-    //    s1
-    //};
-    //
-    //println!("{s2}, world!");
-    //println!("{s1}, world!"); // compile error: s1 is out of scope; Rust call dropp function;
-    //Rust invalidates s1; cannot use it any more
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+                        // println!("after into funciton scope: {s}"); // compile error 's' is moved
 
-    //let mut s = String::from("hello");
-    //s = String::from("ahoy");
-    //
-    //println!("{s}, world!");
+    let x = 5; // x comes into scope
 
-    let mut s = String::from("hello");
-    println!("before: {s}, world!");
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to still
+                   // use x afterward
+    println!("out of scope in simple scalar: {x}") // still work because x is scalar, Rust copy not
+                                                   // move it into function scope
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
 
-    s = String::from("ahoy");
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{some_string}");
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
 
-    println!("after: {s}, world!");
-}
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{some_integer}");
+} // Here, some_integer goes out of scope. Nothing special happens.
