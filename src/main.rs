eb651bf18dca418360c6fcf2ea13c2f0ded77021
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-02 14:08:22 +0700

    borrowing same variable

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
+    let mut s = String::from("hello");
 
-    println!("The len of word {s1} is {len}");
+    //let r1 = &mut s;
+    //let r2 = &mut s;
+    //
+    //println!("{},{}", r1, r2); // cannot 'borrow' "s" more than once at a time
 
-    //let s2 = change(&s1);
+    let r1 = &mut s;
+    println!("r1: {}", r1);
+    let r2 = &mut s;
 
-    let mut s2 = String::from("hello");
-    println!("The value of s2 is {s2}");
-
-    change(&mut s2);
-    println!("The value of s2 is {s2}");
-}
-
-fn calculate_length(s: &String) -> usize {
-    s.len()
-}
-
-//fn change(some_string: &String) {
-//    some_string.push_str(", wolrd");
-//} // compile error cannot change variable that immutable by default
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", wolrd");
+    println!("r2: {}", r2); // work because not 'borrow' more than once at a time
 }
