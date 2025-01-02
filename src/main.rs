b62a5dd2110ec866fda99aab305d0b54350c54d1
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-02 14:51:27 +0700

    borrow multiple immutable

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 fn main() {
-    let mut s = String::from("hello");
+    //let mut s = String::from("hello");
 
     //let r1 = &mut s;
     //let r2 = &mut s;
     //
     //println!("{},{}", r1, r2); // cannot 'borrow' "s" more than once at a time
 
-    let r1 = &mut s;
-    println!("r1: {}", r1);
-    let r2 = &mut s;
+    //let r1 = &mut s;
+    //println!("r1: {}", r1);
+    //let r2 = &mut s;
+    //
+    //println!("r2: {}", r2); // work because not 'borrow' more than once at a time
+    //
+    //{
+    //    let r3 = &s;
+    //    println!("r3: {}", r3);
+    //}
+    //let r4 = &s;
+    //println!("r4: {}", r4);
 
-    println!("r2: {}", r2); // work because not 'borrow' more than once at a time
+    let mut s = String::from("hello");
 
-    {
-        let r3 = &s;
-        println!("r3: {}", r3);
-    }
-    let r4 = &s;
-    println!("r4: {}", r4);
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{r3}");
+
+    //println!("{r1} and {r2}"); compile error, Rust will determin it's ่
 }
