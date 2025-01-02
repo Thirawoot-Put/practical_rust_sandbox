8adf065faaab13ce18f71040116c6dfd675d4553
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-02 13:29:38 +0700

    pass referrence

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+
+    println!("The len of word {s1} is {len}")
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
