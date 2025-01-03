6d1840740afb36dbbb8ccdc0f149af007fc214ed
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-03 11:21:04 +0700

    tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,7 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
-
-    println!("user1 email: {}", user1.email);
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1 // user1.username is moved here
-    };
-    //println!("user1 username: {}", user1.username); // compile error: user1.username was moved
-    println!("user2 email: {}", user2.email);
-}
-
-fn build_user(email: String, username: String) -> User {
-    //User {
-    //    active: true,
-    //    username: username,
-    //    email: email,
-    //    sign_in_count: 1,
-    //};
-
-    User {
-        active: true,
-        username, // short hand
-        email,
-        sign_in_count: 1,
-    }
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
