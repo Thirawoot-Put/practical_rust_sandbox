207ee32e6b69d50e0542e45e6199a1c644ad55f9
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-03 10:18:17 +0700

    update with create new instance from another instance

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,10 @@ fn main() {
         sign_in_count: 1,
     };
 
-    let mut user2 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
     };
-
-    user2.email = String::from("anotheremail@example.com");
 }
 
 fn build_user(email: String, username: String) -> User {
