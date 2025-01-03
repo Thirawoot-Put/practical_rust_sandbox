a35a86191794ab1ec6b14b3b3e6a76b6a7c11090
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-03 10:15:45 +0700

    syntax

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    let mut user2 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user2.email = String::from("anotheremail@example.com");
+}
+
+fn build_user(email: String, username: String) -> User {
+    //User {
+    //    active: true,
+    //    username: username,
+    //    email: email,
+    //    sign_in_count: 1,
+    //};
+
+    User {
+        active: true,
+        username, // short hand
+        email,
+        sign_in_count: 1,
+    }
 }
