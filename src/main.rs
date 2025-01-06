385d98141a07d3d5144aaba7cbb937b2afd7439e
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-06 11:09:38 +0700

    prinln! with struct

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,14 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     hight: u32,
 }
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         hight: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(rect1) // ownership: moved occur here because not pass reference, this pass whole
-                    // instance of struct
-    );
-
-    println!(
-        "The width of the rectangle is {}, area is {}",
-        two_multiply_width(&rect1),
-        area(rect1)
-    ); // not error because width is simple scalar
-
-    println!("Current width {}", rect1.width)
-}
-
-fn area(rectangle: Rectangle) -> u32 {
-    rectangle.width * rectangle.hight
-}
-
-fn two_multiply_width(rectangle: &Rectangle) -> u32 {
-    rectangle.width * 2
+    println!("rect1 is {:#?}", rect1);
 }
