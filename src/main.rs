281418bb87ac95ac3e03e3be6c88814106093917
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-06 10:57:18 +0700

    test ownership

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,20 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        area(rect1) // ownership: moved occur here because not pass reference, this pass whole
+                    // instance of struct
     );
 
     println!(
         "The width of the rectangle is {}, area is {}",
         two_multiply_width(&rect1),
-        area(&rect1)
+        area(rect1)
     ); // not error because width is simple scalar
 
     println!("Current width {}", rect1.width)
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
+fn area(rectangle: Rectangle) -> u32 {
     rectangle.width * rectangle.hight
 }
 
