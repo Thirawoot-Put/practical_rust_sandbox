da3aaada0be7bf62811c49ac882bf45b8e8d3be2
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-07 11:44:41 +0700

    Option standard library

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
+//inside standard library: Option
+//enum Option<T> {
+//    None,
+//    Some(T),
+//}
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
+    let some_number = Some(5); // Rust infer some_number has type Option<i32> **i32 because it's
+                               // default
+    let some_char = Some('e'); // Rust infer some_char has type Option<char>
 
-    let loopback = IpAddr::V6(String::from("::1"));
+    let absent_number: Option<i32> = None;
+
+    println!("{:?}, {:?}, {:?}", some_number, some_char, absent_number);
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    //let sum = x + y; // compile error: they are different type (Option<i8> != i8)
 }
