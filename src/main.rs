d22ebeedb342bfdc6d78709056060e9787e3f63a
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-08 13:33:20 +0700

    match with bind parts of value

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
-    println!("Hello, world!");
+    let quarter = Coin::Quarter(UsState::Alaska);
+    value_in_cents(quarter);
+
+    let penny = Coin::Penny;
+    value_in_cents(penny);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
     }
 }
