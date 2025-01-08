2da5e6b7cdf13bac618c04a4ad47fd5258530b0d
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-08 14:11:35 +0700

    catch-all pattern

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
+    let dice_roll = 9;
 
-    println!("value of six == {:?}", six);
-    println!("value of none == {:?}", none);
-}
-
-fn plus_one(n: Option<i32>) -> Option<i32> {
-    match n {
-        None => None,
-        Some(v) => Some(v + 1),
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
     }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
