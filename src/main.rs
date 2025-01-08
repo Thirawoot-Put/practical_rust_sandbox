809894918eccdaf575b5f4bf2bbf8b843fd08294
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-08 14:06:43 +0700

    match with option

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,15 @@
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
 fn main() {
-    let quarter = Coin::Quarter(UsState::Alaska);
-    value_in_cents(quarter);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 
-    let penny = Coin::Penny;
-    value_in_cents(penny);
+    println!("value of six == {:?}", six);
+    println!("value of none == {:?}", none);
 }
 
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            // bind state => UsState::Alaska
-            println!("State quarter from {state:?}!");
-            25
-        }
+fn plus_one(n: Option<i32>) -> Option<i32> {
+    match n {
+        None => None,
+        Some(v) => Some(v + 1),
     }
 }
