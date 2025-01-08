7c67edf2cbafc38dff9037e069c569634e695406
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-08 14:55:17 +0700

    if let with else

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nikel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    //let config_max = Some(3u8);
-    //match config_max {
-    //    Some(max) => println!("The maximum is configured to be {max}"),
-    //    _ => (),
+    count_none_quarter_coin(Coin::Penny);
+    count_none_quarter_coin(Coin::Quarter(UsState::Alaska));
+}
+
+fn count_none_quarter_coin(coin: Coin) {
+    let mut count = 0;
+
+    //match coin {
+    //    Coin::Quarter(state) => println!("State quarter from {state:?}!"),
+    //    _ => {
+    //        count += 1;
+    //        println!("count: {count}");
+    //    }
     //}
 
-    let config_max = Some(3u8);
-    if let Some(max) = config_max {
-        println!("The maximum is configured to be {max}")
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {state:?}!");
+    } else {
+        count += 1;
+        println!("count: {count}");
     }
 }
