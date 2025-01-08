9fdcc392f66b538d3cfa7a05bd933bbd343a0d3a
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-08 13:34:55 +0700

    explain

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter(state) => {
+            // bind state => UsState::Alaska
             println!("State quarter from {state:?}!");
             25
         }
