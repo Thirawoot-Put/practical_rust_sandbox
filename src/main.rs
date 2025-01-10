d5f4a3e868fe18bad77661df8823ba2f039da274
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-10 10:34:10 +0700

    compile error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+
+mod customer {
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
 }
