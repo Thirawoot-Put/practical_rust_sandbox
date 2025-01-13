9e90fea2a7f322bac0170c7f642183e1e4623615
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-13 17:28:09 +0700

    play push and pop

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,20 @@
 fn main() {
-    let v = vec![1, 2, 3, 4];
+    let mut v = vec![1, 2, 3, 4];
 
-    let third: &i32 = &v[2];
-    println!("Third index of v is {third}");
-
-    let third: Option<&i32> = v.get(2);
-    match third {
-        Some(value) => println!("Third index of v is {value}"),
-        None => println!("There is no third element"),
+    v.push(6);
+    for e in &v {
+        println!("{e}");
     }
 
-    let fifth = v.get(4); // return Option<T>
-    match fifth {
-        Some(value) => println!("Third index of v is {value}"),
-        None => println!("There is no fifth element"),
+    println!("---------------");
+    let a = v.pop();
+    for e in &v {
+        println!("{e}");
     }
 
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
+    println!("---------------");
+    match a {
+        Some(val) => println!("{val}"),
+        None => println!("a is none"),
     }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-
-    {
-        let v = vec![1, 2, 3, 4];
-
-        // do stuff with v
-    } // <- v goes out of scope and is freed here
 }
