7817f93167af17e8e425e86e77e1acbde4bc279d
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-13 13:34:13 +0700

    declare vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
+    let empty_v: Vec<u64> = Vec::new();
+    //empty_v.push(0);
+
+    let fill_v = vec![1, 2, 3];
+    let mut mut_v = Vec::new();
+
+    mut_v.push(1);
+    mut_v.push(2);
+    mut_v.push(3);
+
     println!("Hello, world!");
 }
