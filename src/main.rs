4f2fb0cbd6224771a2150c01eddbbb2a250afb9c
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-14 16:24:39 +0700

    concatenate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let mut s = String::from("foo");
-    let r_s = &s;
-    println!("1---> s: {r_s:p} capacity {}", s.capacity());
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+                       // + is look like this fn add(self, s: &str) -> String {
 
-    let s2 = String::from("bar hello word baba");
-    let r_s2 = &s2;
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
 
-    s.push_str(&s2);
-    let r_s = &s;
-
-    println!("2---> s: {r_s:p} capacity {}", s.capacity());
-    println!("---> s2: {r_s2:p}");
+    //let s = s1 + "-" + &s2 + "-" + &s3;
+    let s = format!("{s1}-{s2}-{s3}"); // returns a String with the contents; uses references
+                                       // (doesn't take ownership of any)
 }
