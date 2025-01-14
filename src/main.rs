37e723764b5db7f33e28a51e898717181163b2f3
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-14 15:55:01 +0700

    string initial value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::new();
+
+    let data = "initial contents";
+
+    s = data.to_string();
+    println!("{s}");
+
+    // the method also works on a literal directly:
+    s = "initial contents".to_string();
+    println!("{s}");
+
+    let s = String::from("hello contents");
+    println!("{s}");
 }
