f174977f1f7c46298635a06ead595cf92ba6fcd9
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-20 13:23:07 +0700

    play string

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
-                       // + is look like this fn add(self, s: &str) -> String {
+    let hello = String::from("Hola");
+    println!("{}", hello.len());
 
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
+    let hello = String::from("Здравствуйте");
+    println!("{}", hello.len());
 
-    //let s = s1 + "-" + &s2 + "-" + &s3;
-    let s = format!("{s1}-{s2}-{s3}"); // returns a String with the contents; uses references
-                                       // (doesn't take ownership of any)
+    let hello = String::from("สวัสดี");
+    println!("{}", hello.len());
 }
