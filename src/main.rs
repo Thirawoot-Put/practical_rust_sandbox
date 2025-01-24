1d0b17b4d331c76c82f820c925dff4fc246372ca
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-24 18:13:54 +0700

    find mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-fn main() {
-    let mut vec = [1, 24, 3, 2, 7, 8, 2];
+use std::collections::HashMap;
 
-    let (_lesser, median, _greater) = vec.select_nth_unstable_by(3, |a, b| b.cmp(a));
+fn main() {
+    let mut v = [1, 24, 3, 2, 7, 8, 2];
+
+    let (_lesser, median, _greater) = v.select_nth_unstable_by(3, |a, b| b.cmp(a));
     println!("median is {median}");
+
+    let mut h = HashMap::new();
+    for n in v {
+        let count = h.entry(n).or_insert(0);
+        *count += 1
+    }
+
+    let mut count_arr = Vec::new();
+    for (_n, count) in h {
+        count_arr.push(count);
+    }
+
+    count_arr.sort();
+    let most = count_arr[count_arr.len() - 1];
+    println!("{most}")
 }
