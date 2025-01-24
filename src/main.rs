d41b7f9079d770fb0259bcdda3d587b03c00c1b9
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-24 17:56:18 +0700

    find median

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut vec = [1, 24, 3, 54, 7, 8, 2];
+
+    let (_lesser, median, _greater) = vec.select_nth_unstable_by(3, |a, b| b.cmp(a));
+    println!("median is {median}");
+
+    vec.sort();
+    println!("{vec:?}")
 }
