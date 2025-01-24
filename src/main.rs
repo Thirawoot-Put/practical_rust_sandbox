d4e7d405b879523dfdf181b2e6818c18c9d29f55
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-24 17:59:02 +0700

    median

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let mut vec = [1, 24, 3, 54, 7, 8, 2];
+    let mut vec = [1, 24, 3, 2, 7, 8, 2];
 
     let (_lesser, median, _greater) = vec.select_nth_unstable_by(3, |a, b| b.cmp(a));
     println!("median is {median}");
-
-    vec.sort();
-    println!("{vec:?}")
 }
